ec6ca54af661653969912958a8df2fc4ce54e362
Feliverse <114632980+Feliverse@users.noreply.github.com>
2023-02-22 12:48:02 -0400

    add components

---
+++ src/components/Calculator.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Display from './Display';
+import Buttons from './Keypad';
+
+const Calculator = () => (
+  <section className="calculator-container">
+    <Display />
+    <Buttons />
+  </section>
+);
+
+export default Calculator;
