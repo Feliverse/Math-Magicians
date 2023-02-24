98b0d695b571585a172af3c592409ed926a446ee
Feliverse <114632980+Feliverse@users.noreply.github.com>
2023-02-23 20:30:21 -0400

    Add HandleClick event

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
 const Display = () => (
-  <div className="calculator-screen">
-    <span className="calculator-display">0</span>
-  </div>
+  
 );
 
 export default Display;
