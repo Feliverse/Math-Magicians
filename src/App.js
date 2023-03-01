3c610fab5a18ddf6d4d3aec98ced973cd03e45b7
Feliverse <114632980+Feliverse@users.noreply.github.com>
2023-03-01 16:02:51 -0400

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
-import Calculator from './components/Calculator';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/routes/Home';
+import Quote from './components/routes/Quote';
+import Calculator from './components/routes/Calculator';
 
 function App() {
   return (
-    <section className="App">
-      <Calculator />
-    </section>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/calculator" element={<Calculator />} />
+      <Route path="/quote" element={<Quote />} />
+    </Routes>
   );
 }
 
---
+++ src/components/routes/Calculator.jsx
@@ -0,0 +1,5 @@
+const Calculator = () => (
+    'Calculator'
+  );
+  
+  export default Calculator;
