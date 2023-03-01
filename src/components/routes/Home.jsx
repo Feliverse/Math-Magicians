3b3a96dbf6d3bd583f721dcff1772ae16c427f0e
Feliverse <114632980+Feliverse@users.noreply.github.com>
2023-03-01 18:32:37 -0400

    fix style

--- src/components/routes/Home.jsx
+++ src/components/routes/Home.jsx
@@ -1,11 +1,11 @@
-import '../styles/Home.css'
+import '../styles/Home.css';
 
 const Home = () => (
-    <section className="home">
-        <h2>Welcome to our page!</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vitae metus in lectus fringilla suscipit nec et magna. Suspendisse non sem id urna elementum ultricies sed a leo. Sed semper pharetra magna, at maximus arcu placerat ut. Ut eleifend ut libero vitae pretium. Mauris aliquam commodo molestie. Donec posuere aliquet feugiat. In eros eros, molestie vel accumsan id, faucibus ut lectus.</p>
-        <p>Nam a dui lorem. Sed at odio velit. Aenean magna sem, semper quis porta nec, tincidunt non ligula. Maecenas at lobortis risus. Quisque volutpat mi id sapien sodales, in iaculis lorem tempor. Vivamus vestibulum maximus mi ac dapibus. Donec rhoncus sem ut eros efficitur feugiat. Integer eu nisl sed lectus imperdiet aliquam. Cras nec vestibulum est, eu convallis nisl.</p>
-    </section>
+  <section className="home">
+    <h2>Welcome to our page!</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    <p>Nam a dui lorem. Sed at odio velit. Aenean magna sem, semper quis porta nec, tincidunt.</p>
+  </section>
 );
 
 export default Home;
--- src/components/routes/Quote.jsx
+++ src/components/routes/Quote.jsx
@@ -1,7 +1,7 @@
-import '../styles/Quote.css'
+import '../styles/Quote.css';
 
 const Quote = () => (
-    <h2 className="quote">"Mathematics is not about numbers, equations, computations, or algorithms: it is about understanding" - William Paul Thurston</h2>
+  <h2 className="quote">Mathematics is not about numbers, equations, computations, or algorithms: it is about understanding - William Paul Thurston</h2>
 );
 
 export default Quote;
