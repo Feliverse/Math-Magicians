541aa754c0e4d889ece11c7952416d1b36613ba5
Feliverse <114632980+Feliverse@users.noreply.github.com>
2023-03-01 17:35:09 -0400

    add pages contend

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import { NavLink } from 'react-router-dom';
+import './styles/Navbar.css';
+
+const links = [
+  { path: '/', text: 'Home' },
+  { path: 'calculator', text: 'Calculator' },
+  { path: 'quote', text: 'Quote' },
+];
+const Navbar = () => (
+  <nav className="navbar">
+    <ul>
+      {links.map((link) => (
+        <li key={link.text}>
+          <NavLink to={link.path}>
+            {link.text}
+          </NavLink>
+        </li>
+      ))}
+    </ul>
+  </nav>
+);
+
+export default Navbar;
